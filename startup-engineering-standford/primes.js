76fb5f4e7bcf44f5054756b3652ba22badcac1f5
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-06-24 23:42:07 -0300

    bump

--- startup-engineering-standford/primes.js
+++ startup-engineering-standford/primes.js
@@ -5,13 +5,20 @@ var outfile = 'primes.txt'
    , out = ''
    , primes = [];
 
-var isPrime = function(number) {
-  if ( number < 2 ) {
+var isPrime = function(n) {
+  if ( n < 2 ) {
     return false;
   }
-  var q = Math.sqrt(number);
+  // If a number n is not a prime, it can be factored into two
+  // factors a and b: n = a*b
+  // If both a and b were greater than the square root of n,
+  // a*b would be greater than n. So at least one of those
+  // factors must be less or equal to the square root of n,
+  // and to check if n is prime, we only need to test for
+  // factors less than or equal to the square root.
+  var q = Math.sqrt(n);
   for (var i = 2; i <= q; i++) {
-    if (number % i == 0) {
+    if (n % i == 0) {
       return false;
     }
   }
